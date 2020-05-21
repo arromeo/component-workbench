c249b724ee0507613dbc38939eb55997e8b78685
Adam Romeo <adam.r.romeo@gmail.com>
2020-05-20 23:14:15 -0700

    Add a bit of styling to the StopStart component

--- src/components/StartStop.js
+++ src/components/StartStop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Box, Button, Typography } from '@material-ui/core'
+import { Box, Button, Grid, Typography } from '@material-ui/core'
 import { fromEvent, interval } from 'rxjs'
 import { exhaustMap, map, takeUntil } from 'rxjs/operators'
 
@@ -13,26 +13,47 @@ export function StartStop() {
     const startClicks = fromEvent(startButtonElem.current, 'click')
     const stopClicks = fromEvent(stopButtonElem.current, 'click')
 
-    const timerObservable = startClicks.pipe(
-      exhaustMap(() =>
-        interval(1000).pipe(
-          takeUntil(stopClicks),
-          map(() => {
-            setTimerDisplay((prev) => prev + 1)
-          })
+    const timerObservable = startClicks
+      .pipe(
+        exhaustMap(() =>
+          interval(1000).pipe(
+            takeUntil(stopClicks),
+            map(() => {
+              setTimerDisplay((prev) => prev + 1)
+            })
+          )
         )
       )
-    )
+      .subscribe()
 
-    const subscription = timerObservable.subscribe()
-    return () => subscription.unsubscribe()
+    return () => timerObservable.unsubscribe()
   }, [])
 
   return (
-    <Box>
-      <Typography>{timerDisplay}</Typography>
-      <Button ref={startButtonElem}>Start</Button>
-      <Button ref={stopButtonElem}>Stop</Button>
+    <Box padding={2}>
+      <Typography variant="h2">{timerDisplay} seconds</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Button
+            ref={startButtonElem}
+            variant="contained"
+            fullWidth
+            disableRipple
+          >
+            Start
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button
+            ref={stopButtonElem}
+            variant="contained"
+            fullWidth
+            disableRipple
+          >
+            Stop
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
