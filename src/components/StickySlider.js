a470ee85ad71fda6cbcab3dd0c6492bc68779b5b
Adam Romeo <adam.r.romeo@gmail.com>
2020-05-31 11:49:54 -0700

    Get slider animations working

--- src/components/StickySlider.js
+++ src/components/StickySlider.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
-import { fromEvent } from 'rxjs'
-import { switchMap, takeUntil, tap } from 'rxjs/operators'
+import { concat, iif, interval, fromEvent } from 'rxjs'
+import { finalize, switchMap, takeUntil, takeWhile, tap } from 'rxjs/operators'
 
 // Components
 import { IconButton } from '@material-ui/core'
@@ -11,7 +11,7 @@ const SliderContainer = styled.div`
   background-color: black;
   width: 800px;
   height: 450px;
-  position: relative;If I re
+  position: relative;
   overflow: hidden;
 `
 
@@ -46,6 +46,9 @@ export function StickySlider() {
   const prevButtonElem = useRef()
   const slideElems = useRef([])
   const currentSlide = useRef(COLORS.length - 1)
+
+  const SLIDE_THRESHOLD_RATIO = 0.2
+  const SNAP_BACK_VELOCITY_PPS = 2200 * (17 / 1000)
 
   const reorderNext = () =>
     slideElems.current.forEach((elem, index) => {
@@ -94,14 +97,73 @@ export function StickySlider() {
       .pipe(
         tap(reorderNext),
         switchMap((mouseDownEvent) =>
-          mouseMoves.pipe(
-            tap((mouseMoveEvent) => {
-              const xDelta = mouseMoveEvent.pageX - mouseDownEvent.pageX
-              if (xDelta < 0)
-                slideElems.current[currentSlide.current].style.left =
-                  xDelta + 'px'
-            }),
-            takeUntil(mouseUps)
+          concat(
+            mouseMoves.pipe(
+              tap((mouseMoveEvent) => {
+                const xDelta = mouseMoveEvent.pageX - mouseDownEvent.pageX
+                if (xDelta < 0)
+                  slideElems.current[currentSlide.current].style.left =
+                    xDelta + 'px'
+              }),
+              takeUntil(mouseUps)
+            ),
+            iif(
+              () => {
+                const currentElem = slideElems.current[currentSlide.current]
+                const currentPosition = currentElem.style.left.slice(0, -2)
+
+                return Math.abs(currentPosition) < 800 * SLIDE_THRESHOLD_RATIO
+              },
+              interval(17).pipe(
+                tap(() => {
+                  const currentElem = slideElems.current[currentSlide.current]
+                  const currentPosition = Number(
+                    currentElem.style.left.slice(0, -2)
+                  )
+
+                  const xDelta = Math.min(
+                    Math.abs(currentPosition),
+                    SNAP_BACK_VELOCITY_PPS
+                  )
+
+                  const newPosition = currentPosition + xDelta
+                  currentElem.style.left = newPosition + 'px'
+                }),
+                takeWhile(() => {
+                  const currentElem = slideElems.current[currentSlide.current]
+                  const currentPosition = currentElem.style.left.slice(0, -2)
+                  return currentPosition < 0
+                })
+              ),
+              interval(17).pipe(
+                tap(() => {
+                  const currentElem = slideElems.current[currentSlide.current]
+                  const currentPosition = Number(
+                    currentElem.style.left.slice(0, -2)
+                  )
+
+                  const xDelta = Math.min(
+                    800 - Math.abs(currentPosition),
+                    SNAP_BACK_VELOCITY_PPS
+                  )
+
+                  currentElem.style.left = currentPosition - xDelta + 'px'
+                }),
+                takeWhile(() => {
+                  const currentElem = slideElems.current[currentSlide.current]
+                  const currentPosition = Number(
+                    currentElem.style.left.slice(0, -2)
+                  )
+                  return Math.abs(currentPosition) < 800
+                }),
+                finalize(() => {
+                  const oldCurrent = slideElems.current[currentSlide.current]
+                  currentSlide.current = currentSlide.current + 1
+                  oldCurrent.style.zIndex = 100
+                  oldCurrent.style.left = '0px'
+                })
+              )
+            )
           )
         )
       )
@@ -111,14 +173,70 @@ export function StickySlider() {
       .pipe(
         tap(reorderPrev),
         switchMap((mouseDownEvent) =>
-          mouseMoves.pipe(
-            tap((mouseMoveEvent) => {
-              const xDelta = mouseDownEvent.pageX + mouseMoveEvent.pageX
-              if (xDelta > 0)
-                slideElems.current[currentSlide.current].style.left =
-                  xDelta + 'px'
-            }),
-            takeUntil(mouseUps)
+          concat(
+            mouseMoves.pipe(
+              tap((mouseMoveEvent) => {
+                const xDelta = mouseDownEvent.pageX + mouseMoveEvent.pageX
+                if (xDelta > 0)
+                  slideElems.current[currentSlide.current].style.left =
+                    xDelta + 'px'
+              }),
+              takeUntil(mouseUps)
+            ),
+            iif(
+              () =>
+                slideElems.current[currentSlide.current].style.left.slice(
+                  0,
+                  -2
+                ) <
+                800 * SLIDE_THRESHOLD_RATIO,
+              interval(17).pipe(
+                tap(() => {
+                  const currentPosition = slideElems.current[
+                    currentSlide.current
+                  ].style.left.slice(0, -2)
+                  const xDelta = Math.min(
+                    currentPosition,
+                    SNAP_BACK_VELOCITY_PPS
+                  )
+                  slideElems.current[currentSlide.current].style.left =
+                    currentPosition - xDelta + 'px'
+                }),
+                takeWhile(
+                  () =>
+                    slideElems.current[currentSlide.current].style.left.slice(
+                      0,
+                      -2
+                    ) > 0
+                )
+              ),
+              interval(17).pipe(
+                tap(() => {
+                  const currentPosition = slideElems.current[
+                    currentSlide.current
+                  ].style.left.slice(0, -2)
+                  const xDelta = Math.min(
+                    800 - currentPosition,
+                    SNAP_BACK_VELOCITY_PPS
+                  )
+                  slideElems.current[currentSlide.current].style.left =
+                    currentPosition + xDelta + 'px'
+                }),
+                takeWhile(
+                  () =>
+                    slideElems.current[currentSlide.current].style.left.slice(
+                      0,
+                      -2
+                    ) < 800
+                ),
+                finalize(() => {
+                  const oldCurrent = slideElems.current[currentSlide.current]
+                  currentSlide.current = currentSlide.current - 1
+                  oldCurrent.style.zIndex = 100
+                  oldCurrent.style.left = '0px'
+                })
+              )
+            )
           )
         )
       )
